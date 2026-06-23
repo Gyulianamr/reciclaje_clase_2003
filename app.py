8a1fb7553d8209d877f2696277a18e9199922625
Gyulianamr <genesismedina0529@gmail.com>
2026-06-23 10:28:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import json
+from pathlib import Path
+import numpy as np
+import steamlit as st
+import tensorflow  as tf
+from PIL import Image
+
+st.set_page_config(page_title="Reciclaje IA-ISC". layout="centered")
+st.title("Modelo predictivo de reciclaje clase de IA-ISC-campus Comayagua-2026")
+st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre-entrenada")
+
 
