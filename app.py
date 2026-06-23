2b7b6774d36d2a59d03585bd6df3223fe0558620
Gyulianamr <genesismedina0529@gmail.com>
2026-06-23 10:28:52 -0600

    Fix syntax error in set_page_config call

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import steamlit as st
 import tensorflow  as tf
 from PIL import Image
 
-st.set_page_config(page_title="Reciclaje IA-ISC". layout="centered")
+st.set_page_config(page_title="Reciclaje IA-ISC", layout="centered")
 st.title("Modelo predictivo de reciclaje clase de IA-ISC-campus Comayagua-2026")
 st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre-entrenada")
 
